3f201df2015e94dd5da08dafe4094b717e72ec01
Mark Probst <mark.probst@gmail.com>
2010-08-04 16:12:54 +0200

    Leiningen

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clojure-flickr-api "0.1"
+  :description "Siteswap Sudoku Generator"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+		 [org.apache.xmlrpc/xmlrpc-client "3.1.3"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
